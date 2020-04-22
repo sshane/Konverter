77221b5024a08a4d5f46a3966b9b8d96e75de8a5
Shane <shane@smiskol.com>
2020-04-21 21:07:50 -0500

    Better format for support classes

--- utils/support.py
+++ utils/support.py
@@ -1,34 +1,73 @@
-class Aliases:
-  class models:
-    sequential = 'sequential'
+class Models:
+  class Sequential:
+    name = 'sequential'
 
-  class layers:
-    dense = 'keras.layers.Dense'
-    dropout = 'keras.layers.Dropout'
-    simplernn = 'keras.layers.SimpleRNN'
 
-  class activations:
-    relu = 'keras.activations.relu'
-    sigmoid = 'keras.activations.sigmoid'
-    softmax = 'keras.activations.softmax'
-    tanh = 'keras.activations.tanh'
-    linear = 'keras.activations.linear'
+class Activations:
+  """
+    The class that contains the activations in string format to be used in the output model file.
+    To add new activations, use the code_converter function and add them here!
+  """
+  class ReLU:
+    name = 'keras.activations.relu'
+    string = 'def relu(x):\n\treturn np.maximum(0, x)'
+
+  class Sigmoid:
+    name = 'keras.activations.sigmoid'
+    string = 'def sigmoid(x):\n\treturn 1 / (1 + np.exp(-x))'
+
+  class Softmax:
+    name = 'keras.activations.softmax'
+    string = 'def softmax(x):\n\treturn np.exp(x) / np.sum(np.exp(x), axis=0)'
+
+  class Tanh:
+    name = 'keras.activations.tanh'
+    string = 'def tanh(x):\n\treturn np.tanh(x)'
+
+  class Linear:  # No activation, but is technically not `None`
+    name = 'keras.activations.linear'
+    string = None
+
+
+class Layers:
+  """
+    The class that contains the layers in string format to be used in the output model file.
+    To add new layers, use the code_converter function and add them here!
+  """
+  class Dense:
+    name = 'keras.layers.Dense'
+    activations = [Activations.ReLU.name, Activations.Sigmoid.name, Activations.Softmax.name, Activations.Tanh.name, Activations.Linear.name]
+    string = 'to_add'
+
+  class Dropout:
+    name = 'keras.layers.Dropout'
+    activations = [None]
+    string = None
+
+  class SimpleRNN:
+    name = 'keras.layers.SimpleRNN'
+    activations = [Activations.Tanh]
+    string = 'def simplernn(x, idx):\n' \
+             '\tstates = [np.zeros(w[idx][0].shape[1], dtype=np.float32)]\n' \
+             '\tfor step in range(x.shape[0]):\n' \
+             '\t\tstates.append(np.tanh(np.dot(x[step], w[idx][0]) + np.dot(states[-1], w[idx][1]) + b[idx]))\n' \
+             '\treturn np.array(states[1:])'
 
 
 class SupportedAttrs:
-  models = ['sequential']
+  models = [Models.Sequential.name]
 
-  layers = [Aliases.layers.dense, Aliases.layers.dropout, Aliases.layers.simplernn]
+  layers = [Layers.Dense.name, Layers.Dropout.name, Layers.SimpleRNN.name]
 
-  activations = [Aliases.activations.relu,
-                 Aliases.activations.sigmoid,
-                 Aliases.activations.softmax,
-                 Aliases.activations.tanh,
-                 Aliases.activations.linear]
+  activations = [Activations.ReLU.name,
+                 Activations.Sigmoid.name,
+                 Activations.Softmax.name,
+                 Activations.Tanh.name,
+                 Activations.Linear.name]
 
-  layers_without_activations = [Aliases.layers.dropout]
-  recurrent_layers = [Aliases.layers.simplernn]
-  ignored_layers = [Aliases.layers.dropout]
+  layers_without_activations = [Layers.Dropout.name]
+  recurrent_layers = [Layers.SimpleRNN.name]
+  ignored_layers = [Layers.Dropout.name]
 
 
 class LayerInfo:
@@ -42,31 +81,6 @@ class LayerInfo:
   activation = None
   weights = None
   biases = None
-
-
-class AttrStrings:
-  """
-    The class that contains the functions and activations in string format to be used in the output model file.
-    To add new functions and activations, use the code_converter function and add them here!
-  """
-  activations = {Aliases.activations.relu: 'def relu(x):\n'
-                                           '\treturn np.maximum(0, x)',
-
-                 Aliases.activations.sigmoid: 'def sigmoid(x):\n'
-                                              '\treturn 1 / (1 + np.exp(-x))',
-
-                 Aliases.activations.softmax: 'def softmax(x):\n'
-                                              '\treturn np.exp(x) / np.sum(np.exp(x), axis=0)',
-
-                 Aliases.activations.tanh: 'def tanh(x):\n'
-                                           '\treturn np.tanh(x)'}
-
-
-  layers = {Aliases.layers.simplernn: 'def simplernn(x, idx):\n'
-                                      '\tstates = [np.zeros(w[idx][0].shape[1], dtype=np.float32)]\n'
-                                      '\tfor step in range(x.shape[0]):\n'
-                                      '\t\tstates.append(np.tanh(np.dot(x[step], w[idx][0]) + np.dot(states[-1], w[idx][1]) + b[idx]))\n'
-                                      '\treturn np.array(states[1:])'}
 
 
 def code_converter(indentation_spaces=2):
