0b449e80ac818fef38c0a2e18650193ecca69781
Shane <shane@smiskol.com>
2020-04-21 20:02:45 -0500

    add code_converter function to help add support for new layers

--- utils/support.py
+++ utils/support.py
@@ -45,12 +45,39 @@ class LayerInfo:
 
 
 class AttrStrings:
-  activations = {Aliases.activations.relu: 'def relu(x):\n\treturn np.maximum(0, x)',
-                 Aliases.activations.sigmoid: 'def sigmoid(x):\n\treturn 1 / (1 + np.exp(-x))',
-                 Aliases.activations.softmax: 'def softmax(x):\n\treturn np.exp(x) / np.sum(np.exp(x), axis=0)',
-                 Aliases.activations.tanh: 'def tanh(x):\n\treturn np.tanh(x)'}
+  """
+    The class that contains the functions and activations in string format to be used in the output model file.
+    To add new functions and activations, use the code_converter function and add them here!
+  """
+  activations = {Aliases.activations.relu: 'def relu(x):\n'
+                                           '\treturn np.maximum(0, x)',
 
-  layers = {Aliases.layers.simplernn: 'def simplernn(x, idx):\n\tstates = [np.zeros(w[idx][0].shape[1], dtype=np.float32)]\n\tfor step in range(x.shape[0]):\n\t\tstates.append(np.tanh(np.dot(x[step], w[idx][0]) + np.dot(states[-1], w[idx][1]) + b[idx]))\n\treturn np.array(states[1:])'}
+                 Aliases.activations.sigmoid: 'def sigmoid(x):\n'
+                                              '\treturn 1 / (1 + np.exp(-x))',
+
+                 Aliases.activations.softmax: 'def softmax(x):\n'
+                                              '\treturn np.exp(x) / np.sum(np.exp(x), axis=0)',
+
+                 Aliases.activations.tanh: 'def tanh(x):\n'
+                                           '\treturn np.tanh(x)'}
+
+
+  layers = {Aliases.layers.simplernn: 'def simplernn(x, idx):\n'
+                                      '\tstates = [np.zeros(w[idx][0].shape[1], dtype=np.float32)]\n'
+                                      '\tfor step in range(x.shape[0]):\n'
+                                      '\t\tstates.append(np.tanh(np.dot(x[step], w[idx][0]) + np.dot(states[-1], w[idx][1]) + b[idx]))\n'
+                                      '\treturn np.array(states[1:])'}
+
+
+def code_converter(indentation_spaces=2):
+  """
+  :param indentation_spaces: Enter the number of spaces for each 'indent'
+  :return: A string representation of the input function
+  """
+  print('This converts code into a supported string format for Konverter.')
+  print('Simply paste your function here, with no extra indentation. Each indent will be replaced with a \\t character.')
+  code = input()
+  return code.replace(' ' * indentation_spaces, '\t')
 
 
 watermark = '"""\n\tGenerated using Konverter: https://github.com/ShaneSmiskol/Konverter\n"""\n\n'
