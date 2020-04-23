3807e837e2e6496435b0c107b025a8d57973d3b6
Shane <shane@smiskol.com>
2020-04-23 17:04:20 -0500

    try without 2

--- tests/test_konverter.py
+++ tests/test_konverter.py
@@ -1,7 +1,7 @@
 import sys
 import os
-# sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
-# print(sys.path)
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+print(sys.path)
 
 from repo_utils.BASEDIR import BASEDIR
 import numpy as np
