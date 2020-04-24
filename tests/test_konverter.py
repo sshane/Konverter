bcd4fbebc1ba0addcf1bd8cac50ae4b444fdb400
Shane Smiskol <shane@smiskol.com>
2020-04-24 03:10:42 -0500

    Add working GitHub Actions support (#2)
    * Test?
    
    * How about this?
    
    * Revert "How about this?"
    
    This reverts commit 2470eb6666a8061437532bf720f59d0b36f50b87.
    
    * run on actions-test branch
    
    * run on all branches
    
    * Remove keras and tensorflow from poetry.lock
    
    * Revert "Remove keras and tensorflow from poetry.lock"
    
    This reverts commit 218a90de60d3b6d24c8a0af65e867b5c7b564f9d.
    
    * test older poetry version
    
    * new new poetry version
    
    * How about this?
    
    * see if this works
    
    * see if this works
    
    * run poetry update
    
    * fix
    
    * add .idea folder
    
    * don't use pytest
    
    * might need this
    
    * debug
    
    * debug
    
    * debug
    
    * no comments?
    
    * test
    
    * test 2
    
    * test 3
    
    * test 4
    
    * test 5
    
    * test 6
    
    * test 6
    
    * test 6
    
    * revert
    
    * use python3
    
    * use python3.7
    
    * test
    
    * test
    
    * test
    
    * test
    
    * test
    
    * test
    
    * test
    
    * try without
    
    * try without 2
    
    * try without 2
    
    * try without 2
    
    * run with poetry!
    
    * now try running file
    
    * try installing poetry properly
    
    * install Python 3.7
    
    * fix for pytest
    
    * fix for pytest
    
    * test
    
    * it works! looks like it keeps the packages in some cache?
    
    * no verbose
    
    * run on different os's and python versions
    
    * allow down to 3.6
    
    * keep testing if one fails
    
    * caret for tensorflow versions
    
    * remove MacOS
    
    * use exact version again
    
    * use beta version
    
    * use beta
    
    * remove Windows from tests
    
    * test only Windows with tf 2.1
    
    * update lock
    
    * remove redundant gitignore lines, ignore generated test files
    
    * try mac
    
    * how about this?
    
    * update version
    
    * update versions
    
    * update version
    
    * test
    
    * test
    
    * test
    
    * test
    
    * test
    
    * test
    
    * back to square one
    
    * back to square one
    
    * This should work
    
    * test 1.14
    
    * update lock
    
    * print errors
    
    * test
    
    * use new tf with py 3.8
    
    * syntax fix
    
    * change order
    
    * test without string
    
    * see if this works
    
    * ?
    
    * !
    
    * test
    
    * test
    
    * test
    
    * test array input
    
    * test another dim
    
    * test no extra dims
    
    * test
    
    * test rc2
    
    * one more dim
    
    * revert back to working without py 3.8 check
    
    * check 10,000 samples
    
    * run on multiple versions of TF
    
    * would this work then?
    
    * install tf 1.14 if that's the version to be tested
    
    * darn pycharm
    
    * better name
    
    * So this shouldn't work... 1.15 should though
    
    * Test TF 1.14, 1.15, 2.0.0b0 against Py 3.6 and 3.7 without making the user install 2.0.0b0
    
    * fix
    
    * fix
    
    * single quotes?
    
    * don't install tensorflow
    
    * don't install tensorflow
    
    * now is this required?
    
    * use appropriate names
    
    * tf 1.7 is too old for 3.7
    
    * skip step with for unsupported combo
    
    * skip step with for unsupported combo
    
    * test
    
    * test
    
    * this might work
    
    * test
    
    * use 1.9
    
    * 1.8 works?
    
    * split jobs up into python versions
    
    * simplify
    
    * test
    
    * test
    
    * test
    
    * test
    
    * test
    
    * test
    
    * test
    
    * test
    
    * test
    
    * test
    
    * test
    
    * test
    
    * test
    
    * test
    
    * test two
    
    * test two
    
    * fix
    
    * see if we can combine these two
    
    * can't combine those two but these should be okay
    
    * nope lol.
    
    * test 1.9, update README with Poetry instructions
    
    * test 1.10
    
    * revert
    
    * test 1.11
    
    * guess this is required 😔
    
    * suppress warnings from tf while testing, test 1.12
    
    * fix
    
    * see if this works
    
    * proper warning suppression
    
    * try this
    
    * probably need this
    
    * test
    
    * remove unneeded
    
    * move \n
    
    * might speed things up
    
    * up 3.6 tf version
    
    * up 3.6 tf version
    
    * up 3.6 tf version
    
    * test
    
    * test
    
    * remove
    
    * update version

--- tests/test_konverter.py
+++ tests/test_konverter.py
@@ -1,34 +1,34 @@
-from repo_utils.BASEDIR import BASEDIR
-import numpy as np
-import time
 import os
-from tensorflow import keras
-from konverter import Konverter
-from tests.build_test_models import create_rnn_model
+import numpy as np
 import importlib
+from konverter import Konverter
+from repo_utils.BASEDIR import BASEDIR
+from tests.build_test_models import create_rnn_model
 
 os.chdir(BASEDIR)
 
 
-def run_tests():
+def test_rnn():
+  print('\nCreating RNN model...')
   ker_rnn_model, shape = create_rnn_model()
-  Konverter(ker_rnn_model, 'tests/rnn_model', 2, verbose=True)
+  Konverter(ker_rnn_model, 'tests/rnn_model', 2, verbose=False)
   kon_rnn_model = importlib.import_module('tests.rnn_model')
 
-  samples = np.random.uniform(0, 10, (100, *shape[1:])).astype('float32')
+  samples = np.random.uniform(0, 10, (10000, *shape[1:])).astype('float32')
   konverter_preds = []
   keras_preds = []
+
+  print('Comparing models...')
   for sample in samples:
     konverter_preds.append(kon_rnn_model.predict(sample)[0])
-    keras_preds.append(ker_rnn_model.predict([[sample]])[0][0])
+    keras_preds.append(ker_rnn_model.predict_on_batch([[sample]])[0][0])
+
   mae = np.mean(np.abs(np.array(keras_preds) - np.array(konverter_preds)))
   mse = np.mean((np.array(keras_preds) - np.array(konverter_preds)) ** 2)
-  print(mae)
-  print(mse)
-  assert mae < 1e-6
-  assert mse < 1e-12
-  print('Keras vs. Konverted model outputs test: successful')
-
-
-if __name__ == '__main__':
-  run_tests()
+  mae_max = 1e-5
+  mse_max = 1e-10
+  assert mae < mae_max
+  assert mse < mse_max
+  print(f'Mean absolute error: {mae} < {mae_max}')
+  print(f'Mean squared error: {mse} < {mse_max}')
+  print('RNN model test passed!')
